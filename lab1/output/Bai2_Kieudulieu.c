a79ca77a27053b70b6064ae220c06ecec81b8c44
QuangMinh <minn10122008@gmail.com>
2026-09-19 04:21:43 +0700

    hoan thanh lab1 bai2

--- lab1/output/Bai2_Kieudulieu.c
+++ lab1/output/Bai2_Kieudulieu.c
@@ -1,14 +1,16 @@
 #include <stdio.h>
 int main() {
-    int namSinh; //%d
+    int namSinh; // %d
     float diemTB; //%f
+
     printf("Nhap nam sinh: ");
     scanf("%d", &namSinh);
-    printf("Nhap diem trung binh:");
+    printf("Nhap diem: ");
     scanf("%f", &diemTB);
-    //xuat
-    printf("Nam Sinh: %d\n", namSinh);
+    // Xuất 
+    printf("Nam sinh: %d\n", namSinh);
     printf("Tuoi: %d\n", 2026 - namSinh);
-    printf("Diem trung binh: %f\n", diemTB);
+    printf("Diem TB: %f", diemTB);
+
     return 0;
 }
